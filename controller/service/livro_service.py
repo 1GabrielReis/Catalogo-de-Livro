50b6dddc93350aefe098510a4a2eec91a7bc1518
Gabriel <greis826@gmail.com>
2026-05-02 21:06:40 -0300

    base service

--- controller/service/livro_service.py
+++ controller/service/livro_service.py
@@ -1,7 +1,28 @@
+from typing import List, TypeVar, Generic
+
 from ...models.entities.livro import Livro
 from ...models.dao.implementation.interface_Dao import Interface_Dao
 
 class Livro_service:
-    def __init__(self, repository:Interface_Dao):
+    def __init__(self, repository:Interface_Dao[Livro]):
         self.repository = repository
     
+
+    def insert(self,livro: Livro) -> int:
+        pass
+
+
+    def update(self,livro: Livro) -> bool:
+        pass
+
+
+    def deleteById(self, id: int) -> bool:
+        pass
+
+
+    def findById(self, id: int) -> Livro:
+        pass
+
+
+    def findAll(self) -> List[Livro]:
+        pass
