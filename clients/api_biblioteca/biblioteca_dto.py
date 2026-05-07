a55ad2b462a5b83a8d3ee2bf79fa6b210bd6fe24
Gabriel <greis826@gmail.com>
2026-05-07 17:08:07 -0300

    remoção do meotdo __repr__

--- clients/api_biblioteca/biblioteca_dto.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field
-
-class Bibliotecas_dto_response(BaseModel):
-    id: int
-    titulo: str = Field(alias="titutlo")
-    autor: str
-    editora: str
--- clients/api_biblioteca/biblioteca_settings.py
+++ clients/api_biblioteca/biblioteca_settings.py
@@ -1,8 +1,4 @@
 class Biblioteca_settings:
-    def __init__(self, base_url, username, parametro = ''):
+    def __init__(self, base_url, username):
         self.base_url: str = base_url 
         self.username: str = username
-        self.parametro: str = parametro
-
-    def __repr__(self):
-        return f'{self.base_url}/{self.username}/{self.parametro}'
