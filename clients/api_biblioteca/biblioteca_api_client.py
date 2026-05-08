e43d27186cf164b2bc9fd838d5b1e363217e88ad
Gabriel <greis826@gmail.com>
2026-05-08 19:28:16 -0300

    alteração na bilioteca de requisição

--- clients/api_biblioteca/biblioteca_api_client.py
+++ clients/api_biblioteca/biblioteca_api_client.py
@@ -1,4 +1,4 @@
-import requests
+import httpx
 
 from .biblioteca_settings import Biblioteca_settings
 from .livro_dto_response import Livro_dto_response
@@ -9,18 +9,19 @@ class biblioteca_api_client:
     def __init__(self, settings: Biblioteca_settings):
         self.settings = settings
 
-    def findById(self,id: int) -> Livro_dto_response:
+    async def findById(self,id: int) -> Livro_dto_response:
         try:
-            url = f'{self.settings.base_url}/{self.settings.username}'
-            response = requests.get(url=url, params={"id": id})
+            async with httpx.AsyncClient() as client:
+                url = f'{self.settings.base_url}/{self.settings.username}'
+                response = await client.get(f'{url}/{id}')
 
-            if response.status_code != 200:
-                raise Biblioteca_exception(f'Erro na requisição:{response.status_code}')
-            
-            data= response.json()
-            return Livro_dto_response(**data)
+                if response.status_code != 200:
+                    raise Biblioteca_exception(f'Erro na requisição:{response.status_code}')
+                
+                data= response.json()
+                return Livro_dto_response(**data)
         
-        except requests.exceptions.RequestException as erro:
+        except httpx.HTTPError as erro:
             raise Biblioteca_exception(f'Erro ao tentar fazer requisção com API biblioteca \ninfo: {erro} ')
         except Exception as erro:
             raise Biblioteca_exception(f'Erro inesperado. Camda de API biblioteca \ninfo: {erro}')
