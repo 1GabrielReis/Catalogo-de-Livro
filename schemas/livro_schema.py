e87237a85630a536d708f24bcd1e163bdf1e6118
Gabriel <greis826@gmail.com>
2026-05-11 16:47:01 -0300

    realocação do schemas

---
+++ schemas/livro_schema.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from datetime import date
+
+from typing import Optional
+
+from ..entities.livro import Livro
+
+class Livro_schema(BaseModel):
+    id: Optional[str]
+    titulo: str
+    autor: str
+    editora: str
+    sobre: Optional[str]
+    data_criacao: Optional[date]
