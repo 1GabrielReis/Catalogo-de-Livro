40e8772e7fcee89f6c29a4388fd04e41eb46b50b
Gabriel <greis826@gmail.com>
2026-05-11 23:13:36 -0300

    metodo insert

--- service/livro_service.py
+++ service/livro_service.py
@@ -4,6 +4,7 @@ from ..models.entities.livro import Livro
 from ..schemas import Livro_schema
 from ..models.dao.implementation.interface_Dao import Interface_Dao
 from ..clients.clients_interface import IBiblioteca_interface, IIa_interface
+from ..clients.api_biblioteca.livro_dto_response import Livro_dto_response
 from .service_exception import Service_Exception
 
 class Livro_service:
@@ -15,14 +16,16 @@ class Livro_service:
         self.ia_client = ia_client
     
 
-    def insert(self,livro_schema: Livro_schema) -> int:
+    async def insert(self,livro_schema: Livro_schema) -> int:
         try:
             livro = Livro(**livro_schema.model_dump())
-            return self.repository.insert(livro)
+            livro = await self._check_library_about(livro)
+
+            await self.repository.insert(livro)
+            return   livro.id
         except Exception as erro:
             raise Service_Exception(f'erro insert service: \ninfo: {erro}')
-
-
+        
 
     def update(self,livro_schema: Livro_schema) -> bool:
         try:
@@ -51,3 +54,9 @@ class Livro_service:
             return self.repository.findAll()
         except Exception as erro:
             raise Service_Exception(f'erro findAll service: \ninfo: {erro}')
+    
+    async def _check_library_about(self,livro: Livro|Livro_dto_response) -> Livro:
+        if not livro.sobre or not livro.sobre.strip():
+            response= await self.ia_client.about_book(livro)
+            livro.sobre = response.sobre
+        return livro
