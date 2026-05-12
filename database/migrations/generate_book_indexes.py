4d831164d8e6746635b0cbf4a12777fc4aab50b8
Gabriel <greis826@gmail.com>
2026-05-12 17:51:55 -0300

    verificação de duas etapa para não ter dados repetido e ajuste no drop 'data_criacao'

--- database/migrations/generate_book_indexes.py
+++ database/migrations/generate_book_indexes.py
@@ -14,7 +14,7 @@ def up(db):
         colecao.create_index('autor')
         colecao.create_index('editora')
         colecao.create_index("data_criacao")
-        colecao.create_index([('titulo',1),('autor',-1)])
+        colecao.create_index([('titulo',1),('autor',-1)],unique=True)
     
     except OperationFailure as erro:
         raise DB_Exception(f"Erro de operação ao criar índices: {str(erro)}")
@@ -28,11 +28,10 @@ def down(db):
         if "Livros" not in db.list_collection_names():
             raise DB_Exception("Coleção 'Livros' não existe. Execute a migration anterior primeiro.")
         
-        colecao.drop_index("_id_1") #o mongo cria o _1 para refereir ao ordem.
-        colecao.drop_index("titulo_1") #sendo 1 crescente ou -1 decrescente)
-        colecao.drop_index("autor_1")        
+        colecao.drop_index("titulo_1") # o mongo cria o _1 para refereir ao ordem.
+        colecao.drop_index("autor_1") # sendo 1 crescente ou -1 decrescente)       
         colecao.drop_index("editora_1")
-        colecao.drop_index("data_criacao")
+        colecao.drop_index("data_criacao_1")
         colecao.drop_index("titulo_1_autor_-1")  # Índice composto
     except OperationFailure as erro:
         raise DB_Exception(f"⚠ Índice não existe ou erro ao remover: {str(erro)}")
