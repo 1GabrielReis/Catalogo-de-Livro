2893e9e84ac179268d4fc4acf57b45cfbd9a5d80
Gabriel <greis826@gmail.com>
2026-05-12 18:00:06 -0300

    renomeção da class

---
+++ models/dao/dao_interface.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import List, TypeVar, Generic
+
+T = TypeVar('T')
+
+class IDao_interface(ABC,Generic[T]):
+    
+    @abstractmethod
+    def insert(self,entity: T):
+        pass
+
+    @abstractmethod
+    def update(self,entity: T) -> bool:
+        pass
+
+    @abstractmethod
+    def deleteById(self, id: str) -> bool:
+        pass
+
+    @abstractmethod
+    def findById(self, id: str) -> T:
+        pass
+
+    @abstractmethod
+    def findAll(self) -> List[T]:
+        pass
