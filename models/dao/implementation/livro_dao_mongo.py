b6d10691bd11a409de4f6f973071fd2bfd41a967
Gabriel <greis826@gmail.com>
2026-06-11 17:09:02 -0300

    remoção do finally no metodo findById

--- models/dao/implementation/livro_dao_mongo.py
+++ models/dao/implementation/livro_dao_mongo.py
@@ -70,20 +70,18 @@ class Livro_dao_mongo(ILivro_interface):
 
 
     def findById(self, id) -> Livro:
-        cursor = None
+        colecao = None
         try:
             livro_id = ObjectId(id)  if isinstance(id, str) else id
         
-            cursor = self.db.getConn()['Livros']
-            livro_dict = cursor.find_one({"_id":livro_id})
-            return self._mapping_entity(livro_dict)
+            colecao = self.db.getConn()['Livros']
+            cursor = colecao.find_one({"_id":livro_id})
+            return self._mapping_entity(cursor)
 
         except errors.OperationFailure as erro:
             raise DB_Exception(f'Erro ao alterar livro \ninfo: {erro}')
         except Exception as erro:
             raise DB_Exception(f'Erro inesperado: \ninfo:{erro}')
-        finally:
-            self.db.closeCursor(cursor)
     
     def findByTitle(self,title: str) -> List[Livro]:
         cursor = None
